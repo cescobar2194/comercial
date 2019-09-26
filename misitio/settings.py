9cfd5bdae357c9fd95798d2f2b3d2720e5a8e179
Carlos Escobar <support@wsg.com.gt>
2019-09-25 18:59:12 -0600

    Se editan los post

--- misitio/settings.py
+++ misitio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yc-r(yrempim8f*c^bg#8_!f#!@t@0+i5+e%gjnt9fns$gwwbc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
